28e75fdd271fd39bec68a0aab8e337664c34097a
Jair_Santana <jair_santana@fgjcdmx.gob.mx>
2026-04-08 12:51:13 -0600

    correcciones de cors

--- src/app/core/env.ts
+++ src/app/core/env.ts
@@ -1,3 +1,8 @@
+const isLocalhost =
+  typeof window !== 'undefined' && window.location.hostname === 'localhost';
+
 export const ENV = {
-  API_URL: 'https://nanoclean-api.onrender.com',
+  API_URL: isLocalhost
+    ? 'http://localhost:3000'
+    : 'https://nanoclean-api.onrender.com',
 };
